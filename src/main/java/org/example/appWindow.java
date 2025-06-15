274eb80b1af26aec8df8b9c7eb086ce1caf338d9
Wiism <legoking1211@gmail.com>
2025-06-15 18:40:03 -0500

    Windows now

---
+++ src/main/java/org/example/appWindow.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import javax.swing.*;
+
+public class appWindow extends  JFrame{
+
+    public appWindow() {
+
+        setTitle("Test");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300, 300);
+        setVisible(true);
+
+    }
+
+}
